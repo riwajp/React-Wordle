4afe4872b03ec245d41320a1a77e7beeacc41cca
riwajp <riwajfictious@gmail.com>
2022-11-26 17:45:07 +0545

    Added page head

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,17 @@
 import "../styles/globals.css";
 import "../styles/index.css";
 import { useState } from "react";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <Head>
+        <title>Wordle</title>
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
 export default MyApp;
