15b08b726fc9837d7c11773f3c629eeedb5987ca
riwajp <riwajfictious@gmail.com>
2022-11-26 18:14:11 +0545

    Added back and new game buttons

---
+++ pages/components/GameButtons.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Link from "next/link";
+
+function GameButtons() {
+  return (
+    <div className="game-buttons-container">
+      <button
+        className="game-buttons-back"
+        onClick={() => location.replace("http://localhost:3000/")}
+      >
+        Back
+      </button>
+      <button
+        className="game-buttons-new-game"
+        onClick={() => location.reload()}
+      >
+        New Game
+      </button>
+    </div>
+  );
+}
+
+export default GameButtons;
